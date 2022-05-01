d8d9fd3615d4007df7f8c1066a698cdbc5a50b18
rinm.desktop <shinzeya@gmail.com>
2022-05-01 18:17:15 +0900

    fix: test-simple storage

--- client/src/component/BCtest.js
+++ client/src/component/BCtest.js
@@ -7,51 +7,76 @@ function BCtest() {
     const [web3, setweb3] = useState(null);
     const [accounts, setaccounts] = useState(null);
     const [contract, setcontract] = useState(null);
+    const [loaded, setloaded] = useState(false)
+    
 
     useEffect(() => {
-        try {
-            const web3 =  getWeb3();
-            // Use web3 to get the user's accounts.
-            const accounts =  web3.eth.getAccounts();
-
-            // Get the contract instance.
-            const networkId =  web3.eth.net.getId();
-            const deployedNetwork = SimpleStorageContract.networks[networkId];
-            const instance = new web3.eth.Contract(
-            SimpleStorageContract.abi,
-            deployedNetwork && deployedNetwork.address,
-            );
-
-            // Set web3, accounts, and contract to the state, and then proceed with an
-            // example of interacting with the contract's methods.
-            setweb3(web3);
-            setaccounts(accounts);
-            setcontract(instance);
-            runExample();
-        } catch (error) {
-            // Catch any errors for any of the above operations.
-            alert(
-            `Failed to load web3, accounts, or contract. Check console for details.`,
-            );
-            console.error(error);
+        const runExample= async()=>{
+            if(loaded){
+                console.log(web3);
+                // Stores a given value, 5 by default.
+                await contract.methods.set(5).send({ from: accounts[0] });
+                // Get the value from the contract to prove it worked.
+                const response = await contract.methods.get().call();
+                setstorageValue(response);
+            }
+            
         }
-    }, [])
+        const getMeta = async ()=>{
+            try {
+                const _web3 =  await getWeb3();
+                // Use web3 to get the user's accounts.
+                const _accounts = await _web3.eth.getAccounts();
+                // console.log(accounts);
+                // Get the contract instance.
+                const networkId = await _web3.eth.net.getId();
+                const deployedNetwork = await SimpleStorageContract.networks[networkId];
+                // console.log(SimpleStorageContract);
+                const _instance = new _web3.eth.Contract(
+                SimpleStorageContract.abi,
+                deployedNetwork && deployedNetwork.address,
+                );
+                // console.log(instance);
     
-    const runExample = async () => {
+                // // Set web3, accounts, and contract to the state, and then proceed with an
+                // // example of interacting with the contract's methods.
+                setweb3(_web3);
+                setaccounts(_accounts);
+                setcontract(_instance);
+                setloaded(true);
+            } catch (error) {
+                // Catch any errors for any of the above operations.
+                console.error(error);
+            }
+        }
+        
+        getMeta();
+        runExample();
+        
+
+    }, [loaded]);
+
     
-        // Stores a given value, 5 by default.
-        await contract.methods.set(5).send({ from: accounts[0] });
-    
-        // Get the value from the contract to prove it worked.
-        const response = await contract.methods.get().call();
-    
-        // Update state with the result.
-        setstorageValue(response);
-    };
+    const render=()=>{
+        if(web3===null){
+            return(
+                <div>
+                    loading...
+                </div>
+            )
+        }else{
+            return(
+                <div>
+                    {storageValue}
+                </div>
+            )
+        }
+        
+    }
 
 
     return (
-    <div>BCtest</div>
+        render()
     )
 }
 
